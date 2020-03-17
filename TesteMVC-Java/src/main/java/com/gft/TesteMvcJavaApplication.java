603ff77ef3cdd37782bd2660803342e606d14bb0
BrunoAtaulo <b.begliomini@gmail.com>
2020-03-17 15:47:14 -0300

    Teste de MVC no Java

---
+++ TesteMVC-Java/src/main/java/com/gft/TesteMvcJavaApplication.java
@@ -0,0 +1,13 @@
+package com.gft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteMvcJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesteMvcJavaApplication.class, args);
+	}
+
+}

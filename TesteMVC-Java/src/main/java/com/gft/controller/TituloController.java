2875652bd69181b2e459e7903ec298fe8e1260f7
BrunoAtaulo <b.begliomini@gmail.com>
2020-03-18 15:52:40 -0300

    Usando thymeleaf 18-03-2020

--- TesteMVC-Java/src/main/java/com/gft/controller/TituloController.java
+++ TesteMVC-Java/src/main/java/com/gft/controller/TituloController.java
@@ -1,12 +1,18 @@
 package com.gft.controller;
 
+import java.util.Arrays;
+import java.util.List;
+
+import com.gft.model.StatusTitulo;
 import com.gft.model.Titulo;
 import com.gft.repository.Titulos;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 //import jdk.nashorn.internal.ir.RuntimeNode.Request;
 
@@ -18,14 +24,29 @@ public class TituloController {
 	private Titulos titulos;
 
 	@RequestMapping("/novo")
-	public String novo() {
-		return "CadastroTitulo";
+	public ModelAndView novo() {
+		ModelAndView mv = new ModelAndView("CadastroTitulo");
+		mv.addObject("todosStatusTitulo", StatusTitulo.values());
+		return mv;
 	}
 
 	@RequestMapping(method = RequestMethod.POST)
-	public String salvar(Titulo titulo){
-
+	public ModelAndView salvar(Titulo titulo){
 		titulos.save(titulo);
-		return "CadastroTitulo";
+
+		ModelAndView mv = new ModelAndView("CadastroTitulo");
+		mv.addObject("mensagem", "Titulo salvo com sucesso!");
+		mv.addObject("todosStatusTitulo", StatusTitulo.values());
+		return mv;
+	}
+
+	@RequestMapping
+	public String pesquisar(){
+		return "PesquisaTitulos";
+	}
+
+	@ModelAttribute("todosStatusTitulo")
+	public List<StatusTitulo> todosStatusTitulo(){
+		return Arrays.asList(StatusTitulo.values());
 	}
 }
--- TesteMVC-Java/src/main/java/com/gft/model/Titulo.java
+++ TesteMVC-Java/src/main/java/com/gft/model/Titulo.java
@@ -13,6 +13,8 @@ import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 
 import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.format.annotation.NumberFormat;
+
 
 @Entity
 public class Titulo {
@@ -27,8 +29,9 @@ public class Titulo {
 	@Temporal(TemporalType.DATE)
 	private Date dataVencimento;
 
+	@NumberFormat(pattern = "#,##0.00")
 	private BigDecimal valor;
-
+	
 	@Enumerated(EnumType.STRING)
 	private StatusTitulo status;
 	

690c8f1242287031d8baabd30b9e10da30f25f58
BrunoAtaulo <b.begliomini@gmail.com>
2020-03-20 12:34:09 -0300

    Projeto dia 20-03 - manha

--- TesteMVC-Java/src/main/java/com/gft/controller/TituloController.java
+++ TesteMVC-Java/src/main/java/com/gft/controller/TituloController.java
@@ -1,7 +1,6 @@
 package com.gft.controller;
 
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 import com.gft.model.StatusTitulo;
 import com.gft.model.Titulo;
@@ -12,37 +11,37 @@ import org.springframework.stereotype.Controller;
 import org.springframework.validation.Errors;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
-
-
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/titulos")
 public class TituloController {
+
+	private static final String CADASTRO_VIEW = "CadastroTitulo";
 
 	@Autowired
 	private Titulos titulos;
 
 	@RequestMapping("/novo")
 	public ModelAndView novo() {
-		ModelAndView mv = new ModelAndView("CadastroTitulo");
+		ModelAndView mv = new ModelAndView(CADASTRO_VIEW);
 		mv.addObject(new Titulo());
 		return mv;
 	}
 
 	@RequestMapping(method = RequestMethod.POST)
-	public ModelAndView salvar(@Validated Titulo titulo, Errors errors) {
-		ModelAndView mv = new ModelAndView("CadastroTitulo");
+	public String salvar(@Validated Titulo titulo, Errors errors, RedirectAttributes attributes) {
 		if (errors.hasErrors()) {
-			return mv;
+			return CADASTRO_VIEW;
 		}
-		
+
 		titulos.save(titulo);
-		
-		mv.addObject("mensagem", "Titulo salvo com sucesso!");
-		return mv;
+		attributes.addFlashAttribute("mensagem", "Título salvo com sucesso");
+		return "redirect:/titulos/novo";
 	}
 
 	@RequestMapping
@@ -53,8 +52,16 @@ public class TituloController {
 		return mv;
 	}
 
+	@RequestMapping(value="{codigo}")
+	public ModelAndView edicao(@PathVariable("codigo") Titulo titulo) {
+		ModelAndView mv = new ModelAndView(CADASTRO_VIEW);
+		mv.addObject(titulo);
+		return mv;
+	}
+
 	@ModelAttribute("todosStatusTitulo")
 	public List<StatusTitulo> todosStatusTitulo() {
 		return Arrays.asList(StatusTitulo.values());
 	}
+
 }
